8edccc16d88b96650f11cda8a745b1498b967fbb
EkaterinaIlina <51483385+EkaterinaIlina@users.noreply.github.com>
2021-05-30 01:03:58 +0300

    все лабораторные

--- labsOOP/FormLab8.Designer.cs
+++ labsOOP/FormLab8.Designer.cs
@@ -28,19 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBoxPicture = new System.Windows.Forms.ComboBox();
+            this.numericUpDownDepth = new System.Windows.Forms.NumericUpDown();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonPaint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDepth)).BeginInit();
             this.SuspendLayout();
+            // 
+            // comboBoxPicture
+            // 
+            this.comboBoxPicture.FormattingEnabled = true;
+            this.comboBoxPicture.Items.AddRange(new object[] {
+            "Звезда",
+            "Снеговик",
+            "Дом"});
+            this.comboBoxPicture.Location = new System.Drawing.Point(206, 36);
+            this.comboBoxPicture.Name = "comboBoxPicture";
+            this.comboBoxPicture.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPicture.TabIndex = 0;
+            this.comboBoxPicture.SelectedIndexChanged += new System.EventHandler(this.comboBoxPicture_SelectedIndexChanged);
+            // 
+            // numericUpDownDepth
+            // 
+            this.numericUpDownDepth.Location = new System.Drawing.Point(206, 87);
+            this.numericUpDownDepth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDepth.Name = "numericUpDownDepth";
+            this.numericUpDownDepth.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownDepth.TabIndex = 2;
+            this.numericUpDownDepth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDepth.ValueChanged += new System.EventHandler(this.comboBoxPicture_SelectedIndexChanged);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(205, 179);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(121, 23);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonPaint
+            // 
+            this.buttonPaint.Location = new System.Drawing.Point(206, 148);
+            this.buttonPaint.Name = "buttonPaint";
+            this.buttonPaint.Size = new System.Drawing.Size(121, 23);
+            this.buttonPaint.TabIndex = 4;
+            this.buttonPaint.Text = "Нарисовать";
+            this.buttonPaint.UseVisualStyleBackColor = true;
+            this.buttonPaint.Click += new System.EventHandler(this.buttonPaint_Click);
             // 
             // FormLab8
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 355);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(352, 234);
+            this.Controls.Add(this.buttonPaint);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.numericUpDownDepth);
+            this.Controls.Add(this.comboBoxPicture);
             this.Name = "FormLab8";
             this.Text = "Лабораторная работа №8";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDepth)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.ComboBox comboBoxPicture;
+        private System.Windows.Forms.NumericUpDown numericUpDownDepth;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonPaint;
     }
 }
--- labsOOP/FormLab8.cs
+++ labsOOP/FormLab8.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace labsOOP
@@ -15,6 +10,96 @@ namespace labsOOP
         public FormLab8()
         {
             InitializeComponent();
+            comboBoxPicture.SelectedIndex = 0;
+        }
+        Graphics graphics;
+
+        private Point[] star5(int x, int y, int r)  // звезда
+        {
+            Point point1 = new Point(x, y - 6 * r);
+            Point point2 = new Point(x + 2 * r, y - 3 * r);
+            Point point3 = new Point(x + 6 * r, y - 2 * r);
+            Point point4 = new Point(x + 3 * r, y + r);
+            Point point5 = new Point(x + 4 * r, y + 5 * r);
+            Point point6 = new Point(x, y + 3 * r);
+            Point point7 = new Point(x - 4 * r, y + 5 * r);
+            Point point8 = new Point(x - 3 * r, y + r);
+            Point point9 = new Point(x - 6 * r, y - 2 * r);
+            Point point10 = new Point(x - 2 * r, y - 3 * r);
+            Point[] pm =
+            { point1,point2,point3,point4,point5,
+             point6,point7,point8,point9,point10 };
+            return pm;
+        }
+
+        private void comboBoxPicture_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxPicture.SelectedIndex == 0)
+                numericUpDownDepth.Enabled = true;
+            else if (comboBoxPicture.SelectedIndex == 1)
+                numericUpDownDepth.Enabled = false;
+            else if (comboBoxPicture.SelectedIndex == 2)
+                numericUpDownDepth.Enabled = true;
+        }
+    
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.WhiteSmoke);
+        }
+
+        private void buttonPaint_Click(object sender, EventArgs e)
+        {
+            graphics = this.CreateGraphics();
+            graphics.Clear(Color.WhiteSmoke);
+            if (comboBoxPicture.SelectedIndex == 0)
+            {
+                ColorDialog colorDialogStar = new ColorDialog();
+                MessageBox.Show("Выберите цвет звезды");
+                Pen pen;
+                if (colorDialogStar.ShowDialog() == DialogResult.OK)
+                    pen = new Pen(colorDialogStar.Color, (float)numericUpDownDepth.Value);
+                else
+                    pen = new Pen(Color.Yellow, (float)numericUpDownDepth.Value);
+                // Задание 10 точек, определяющих замкнутую область "звезда"
+                Point[] pm = star5(110, 100, 12);
+                // Рисование замкнутой ломаной из отрезков
+                graphics.DrawClosedCurve(pen, pm, 0, FillMode.Alternate);
+            }
+            else if (comboBoxPicture.SelectedIndex == 1)
+            {
+                SolidBrush solidBrush;
+                ColorDialog colorDialogSnowman = new ColorDialog();
+                MessageBox.Show("Выберите цвет снеговика");
+                if (colorDialogSnowman.ShowDialog() == DialogResult.OK)
+                    solidBrush = new SolidBrush(colorDialogSnowman.Color);
+                else solidBrush = new SolidBrush(Color.Azure);
+                int size = 40;
+                graphics.FillEllipse(solidBrush, 40, 10, Convert.ToSingle(size * 1.5), Convert.ToSingle(size * 1.5));
+                graphics.FillEllipse(solidBrush, (int)(50 - size / 2), 20 + size, 2 * size, 2 * size);
+                graphics.FillEllipse(solidBrush, 50 - size, 3 * size, 3 * size, 3 * size);
+                var points = new[]{new Point (35, 40 ), new Point(60, 35), new Point(60,  45) };
+                 solidBrush = new SolidBrush(Color.Orange);
+                graphics.FillPolygon(solidBrush, points);
+            }
+            else if (comboBoxPicture.SelectedIndex == 2)
+            {
+                ColorDialog colorDialogHouse = new ColorDialog();
+                MessageBox.Show("Выберите цвет дома");
+                Pen pen;
+                if (colorDialogHouse.ShowDialog() == DialogResult.OK)
+                  pen = new Pen(colorDialogHouse.Color, (float)numericUpDownDepth.Value);
+                else
+                    pen = new Pen(Color.Black, (float)numericUpDownDepth.Value);
+                graphics.DrawLine(pen, 45, 20, 15, 70);
+                graphics.DrawLine(pen, 45, 20, 75, 70);
+                graphics.DrawLine(pen, 15, 70, 75, 70);
+                graphics.DrawLine(pen, 45, 20, 150, 20);
+                graphics.DrawLine(pen, 150, 20, 185, 70);
+                graphics.DrawLine(pen, 75, 70, 185, 70);
+                graphics.DrawRectangle(pen, 15, 70, 60, 80);
+                graphics.DrawRectangle(pen, 75, 70, 110, 80);
+            }
         }
     }
 }
